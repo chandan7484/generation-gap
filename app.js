071bb5b4fba59292c31eb5db5df0783104dc5b72
Rupali0311 <rupalitiwary0311@gmail.com>
2021-01-16 00:37:58 +0530

    enquiry form

--- app.js
+++ app.js
@@ -8,12 +8,17 @@ const exphbs = require("express-handlebars");
 const app = express()
 
 const User=require("./models/User")
+const sgMail = require('@sendgrid/mail')
 
-
-
+sgMail.setApiKey(API_KEY)
+// const db ='mongodb://localhost:27017/glorifydb'
 const db ='mongodb+srv://@password:online@cluster0.wkx7c.mongodb.net/User?retryWrites=true&w=majority'
 
-
+mongoose.connect(db,  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).then(() => console.log('MongoDB Connected....'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname+"/public")));
@@ -30,7 +35,12 @@ app.use('/user', require('./routes/user'));
 
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname+"/public/views/home.html"))
+  res.render((path.join(__dirname+"/public/views/home")),{layout:false});
+})
+
+
+app.get("/login",(req,res)=>{
+  res.render((path.join(__dirname+'/public/views/login')), { layout: false });
 })
 
 app.post('/login', (req, res) => {
@@ -50,9 +60,11 @@ app.post('/login', (req, res) => {
 
 
 
-  res.redirect('/')
+  res.redirect('/after_login')
 })
-
+app.get("/after_login", function(req, res) {
+  res.render((path.join(__dirname+'/public/views/home2')), { layout: false });
+})
 
 
 
@@ -60,10 +72,14 @@ app.get("/register", function(req, res) {
   res.render((path.join(__dirname+'/public/views/register')), { layout: false });
 })
 app.get("/enquiry", function(req, res) {
-  res.sendFile(path.join(__dirname+"/public/views/enquiry.html"))
+  res.render((path.join(__dirname+"/public/views/enquiry")), { layout: false });
 })
-
-
+app.get("/about", function(req, res) {
+  res.render((path.join(__dirname+"/public/views/about")), { layout: false });
+})
+app.get("/aboutL", function(req, res) {
+  res.render((path.join(__dirname+"/public/views/aboutL")), { layout: false });
+})
 
 app.post('/register', async (req, res) => {
   bcrypt.genSalt(10, function (err, Salt) {   
@@ -81,10 +97,44 @@ app.post('/register', async (req, res) => {
           })
       })
   })
-res.redirect('/')
+res.redirect('/login')
 })
 
+app.post('/enquiry',(req,res)=>{
+  const msg = {
+    to: 'hackingnitp@gmail.com', // Change to your recipient
+    from: 'nitphack@gmail.com', // Change to your verified sender
+    subject: 'A new enquiry posted regarding '+req.body.subject+'.',
+    text: 'An enquiry has been posted by '+req.body.name+'. The question of concern is '+req.body.question+' .Please try to reach out to him/her using the email '+req.body.email+'.'
+    // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+console.log(req.body.email)
+  const msg2 = {
+    to: req.body.email, // Change to your recipient
+    from: 'nitphack@gmail.com', // Change to your verified sender
+    subject: 'Regarding your enquiry',
+    text: 'Thank you for posting your query. We will try answering it soon.',
+    // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  }
+  sgMail
+  .send(msg2)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
 
+res.redirect('/after_login')
+})
 
 
 
